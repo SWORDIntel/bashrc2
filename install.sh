57f0878a872047a1d53e5d01da379207b92a6981
David England <117012829+parttimeamerican@users.noreply.github.com>
2024-10-05 22:43:39 +0100

    Added daily sync 4real

--- install.sh
+++ install.sh
@@ -1,63 +1,69 @@
-#!/bin/bash
-# all I see turns to brown; as the sun turns to brown
-for old in .bashrc .bash_aliases .bash_completion .bash_functions; do
-	if [ -f "${HOME}/${old}" ]; then
-		echo "Found existing ${old}, backing up to ${old}.bak"
-		mv ${HOME}/${old} ${HOME}/${old}.bak
-	fi
-done
+	#!/bin/bash
+	# Shamelessly ripped from the work of a much greater man,then durandal jacked it and i traded #it for some crack...then here we are
+	for old in .bashrc .bash_aliases .bash_completion .bash_functions; do
+		if [ -f "${HOME}/${old}" ]; then
+			echo "Found existing ${old}, backing up to ${old}.bak"
+			mv ${HOME}/${old} ${HOME}/${old}.bak
+		fi
+	done
 
-for new in bashrc bash_aliases bash_functions bash_completion bash_modules; do
-	if [ -f "./${new}" ]; then
-		echo "Installing ${new}"
-		cp -v "./${new}" "${HOME}/.${new}" 2>/dev/null | awk '{print "\t" $0}'
-
-	fi
-done
-
-unset $new
-
-for new in bashrc.precustom bashrc.postcustom; do
-	if [ -f "./${new}" ]; then
-		if [ -f "${HOME}/.${new}" ]; then
-			loop=1
-			while [ $loop == 1 ]; do
-				read -p "Overwrite ${HOME}/.${new} [Y/n] " overwrite
-				case "$overwrite" in
-					'Y'|'y'|'yes')
-						cp -v "./${new}" "${HOME}/.${new}" 2>/dev/null | awk '{print "\t" $0}'
-						loop=0
-					;;
-
-					'N'|'n'|'no')
-						echo "No"
-						loop=0
-					;;
-
-					*)
-						echo "Type 'yes' or 'no' (I'm lazy)"
-					;;
-
-				esac
-				unset $overwrite
-
-			done
-		else 
+	for new in bashrc bash_aliases bash_functions bash_completion bash_modules; do
+		if [ -f "./${new}" ]; then
 			echo "Installing ${new}"
 			cp -v "./${new}" "${HOME}/.${new}" 2>/dev/null | awk '{print "\t" $0}'
+
 		fi
-	fi
-done
+	done
 
-for newd in bash_aliases bash_functions bash_completion bash_modules; do
-	if [ ! -d "${HOME}/.${newd}.d}" ]; then
-		echo "Creating ${HOME}/.${newd}.d:"
+	unset $new
 
-		mkdir -pv "${HOME}/.${newd}.d"
-		chmod 0700 "${HOME}/.${newd}.d"
-	fi
+	for new in bashrc.precustom bashrc.postcustom; do
+		if [ -f "./${new}" ]; then
+			if [ -f "${HOME}/.${new}" ]; then
+				loop=1
+				while [ $loop == 1 ]; do
+					read -p "Overwrite ${HOME}/.${new} [Y/n] " overwrite
+					case "$overwrite" in
+						'Y'|'y'|'yes')
+							cp -v "./${new}" "${HOME}/.${new}" 2>/dev/null | awk '{print "\t" $0}'
+							loop=0
+						;;
 
-	if [ -d "${HOME}/.${newd}.d" ]; then
-		cp -rv ./${newd}.d/* ${HOME}/.${newd}.d/ 2>/dev/null | awk '{print "\t" $0}'
-	fi
-done
+						'N'|'n'|'no')
+							echo "No"
+							loop=0
+						;;
+
+						*)
+							echo "Type 'yes' or 'no' (I'm lazy)"
+						;;
+
+					esac
+					unset $overwrite
+
+				done
+			else 
+				echo "Installing ${new}"
+				cp -v "./${new}" "${HOME}/.${new}" 2>/dev/null | awk '{print "\t" $0}'
+			fi
+		fi
+	done
+
+	for newd in bash_aliases bash_functions bash_completion bash_modules; do
+		if [ ! -d "${HOME}/.${newd}.d}" ]; then
+			echo "Creating ${HOME}/.${newd}.d:"
+
+			mkdir -pv "${HOME}/.${newd}.d"
+			chmod 0700 "${HOME}/.${newd}.d"
+		fi
+
+		if [ -d "${HOME}/.${newd}.d" ]; then
+			cp -rv ./${newd}.d/* ${HOME}/.${newd}.d/ 2>/dev/null | awk '{print "\t" $0}'
+		fi
+if [ -f "./sync.sh" ]; then
+    echo "Running sync.sh..."
+    ./sync.sh
+else
+    echo "sync.sh not found in the current directory."
+fi
+	done
